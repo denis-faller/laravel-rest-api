6ad70ea79f0e4643b2534bdc31a30f535cdb2f1c
Denis Faller <richsiteru@gmail.com>
2020-01-31 16:34:56 +0700

    authorization api

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', 'Api\AuthController@register')->name('register');
+Route::post('/login', 'Api\AuthController@login')->name('login');
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('/games', 'GameController');
+    Route::get('/logout', 'Api\AuthController@logout')->name('logout');
 });
-
-Route::apiResource('/games', 'GameController');
